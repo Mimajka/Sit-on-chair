c6e11d4bc79bc9e1366016dff410fa9e0e9ee224
Marta Majewska <m.marta.majewska@gmail.com>
2018-01-31 20:12:21 +0100

    Slider

--- js/app.js
+++ js/app.js
@@ -21,20 +21,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
     for (var i = 0; i < imgProduct.length; i++) {
         imgProduct[i].addEventListener("mouseover", function(){
-
             var label = this.nextElementSibling;
             label.style.display ="none";
-
         });
-
         imgProduct[i].addEventListener("mouseout", function(){
 
             var label = this.nextElementSibling;
             label.style.display ="block";
-
         })
-
-
     }
+
+    var prevPicture = document.querySelector('.less-than');
+    var nextPicture = document.querySelector('.greater-than');
+
+    var slides = document.querySelectorAll('.slider li');
+    var index = 0;
+
+    console.log(slides);
+
+    slides[index].classList.add("visible");
+
+    nextPicture.addEventListener("click", function() {
+        slides[index].classList.remove("visible");
+        index++;
+        index %= slides.length;
+        slides[index].classList.add('visible');
+
+
+    });
+
+    prevPicture.addEventListener("click", function() {
+        slides[index].classList.remove("visible");
+        index--;
+        index = index <0 ? slides.length -1 :index;
+
+
+        slides[index].classList.add('visible');
+
+
+    })
+
 
 });
